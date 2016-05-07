84951487b0e3a5b23374562ae2aa2fada41c73f1
Alex <Alex@Alexander>
2016-05-07 16:43:15 -0500

    aumentado los libros para buscar para la edicion y en al interfaz de edicion echo el buscar en los libros y pasar una fila de una palabra buscada para arriba

--- ui/QtNLP_Wordnet.py
+++ ui/QtNLP_Wordnet.py
@@ -3,14 +3,14 @@
 
 # main core file
 from string import lower
-
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from QtNLP_Wordnet_UI import Ui_MainWindow
-import sqlite3
+from QtNLP_Wordnet_Editor_UI import Ui_MainWindow as df
+import sqlite3,sys
 
 
-class Wordnet( QMainWindow, Ui_MainWindow):
+class Wordnet(QMainWindow, Ui_MainWindow):
 
     def __init__( self, parent = None ):
         # init the parent
@@ -24,11 +24,12 @@ class Wordnet( QMainWindow, Ui_MainWindow):
         self.__max_row = 0
         self.__column = 1
 
-        self.pushButton.clicked.connect( self.insertRow )
+        self.pushButton.clicked.connect( self.insertRow)
         self.comboBox.editTextChanged.connect( self.actualizar )
 
         self.pushButton_2.clicked.connect( self.insertSynonyms)
         self.pushButton_3.clicked.connect( self.insertAntonyms)
+        self.pushButton_4.clicked.connect( self.mostar)
 
     # SLOTS !!!
     def actualizar(self):
@@ -174,7 +175,7 @@ class Wordnet( QMainWindow, Ui_MainWindow):
             result = self.__cursor.fetchall()
             if result:
                 for w in result:
-                    if(self.comboBox.currentText().toLower()!=lower(w[0])):
+                    if(self.comboBox.currentText().toLower() != lower(w[0])):
                         self.listWidget.addItem(w[0])
 
 
@@ -214,7 +215,7 @@ class Wordnet( QMainWindow, Ui_MainWindow):
                   for a in data:
                         self.listWidget.addItem(a[0])
 
-            str = "SELECT DISTINCT([data_adv_ptr].[synset_offset_p]) FROM [index_sense]  INNER JOIN [data_adv_ptr] USING([synset_offset]) WHERE [index_sense].[lemma]='"+self.comboBox.currentText()+"'AND [data_adv_ptr].[pointer_symbol]='!'"
+            str = "SELECT DISTINCT([data_adv_ptr].[synset_offset_p]) FROM [index_sense]  INNER JOIN [data_adv_ptr] USING([synset_offset]) WHERE [index_sense].[lemma]='" + self.comboBox.currentText() + "'AND [data_adv_ptr].[pointer_symbol]='!'"
             self.__cursor.execute(unicode(str))
             result = self.__cursor.fetchall()
             if result:
@@ -258,3 +259,319 @@ class Wordnet( QMainWindow, Ui_MainWindow):
             #if result:
             #    for w in result:
             #      self.listWidget.addItem(w[0])
+    def mostar(self):
+        #Wordnet()
+        edit = QApplication( sys.argv )
+        edit.exit()
+        e = WordnetEdit()
+        e.show()
+        sys.exit( edit.exec_() )
+
+class WordnetEdit( QMainWindow, df):
+
+    def __init__( self, parent = None ):
+        # init the parent
+        super( WordnetEdit, self ).__init__( parent )
+        # setup the UI
+        self.setupUi( self )
+
+        self.__connect = sqlite3.connect("./data/wordnet.db3")
+        self.__cursor = self.__connect.cursor()
+
+        self.__bbook = []
+        self.__filasel = []
+        self.__filword = []
+        self.__allsynon = []
+
+        self.__max_row1 = 0
+        self.__row1 = 0
+        self.__column1 = 1
+
+        self.__max_row2 = 0
+        self.__row2 = 0
+        self.__column2 = 1
+
+        self.pushButton_2.clicked.connect( self.insertRow1)
+        self.comboBox_2.editTextChanged.connect( self.actualizar1 )
+        self.comboBox_3.editTextChanged.connect( self.actualizar2 )
+        self.pushButton_3.clicked.connect( self.insertRow2)
+        self.pushButton.clicked.connect( self.buscarBook)
+        self.listWidget_3.activated.connect(self.Cargar)
+        self.lineEdit_3.textEdited.connect(self.actualizarbook)
+        self.tableWidget_2.cellClicked.connect(self.seleccionar)
+        self.pushButton_4.clicked.connect(self.upsense)
+        self.pushButton_9.clicked.connect(self.insertSynonyms)
+
+
+
+    def actualizar1(self):
+        cant_row = self.tableWidget.rowCount()
+        self.listWidget.clear()
+        if(cant_row!=-0):
+            while(cant_row!=0):
+               self.tableWidget.removeRow(cant_row)
+               cant_row -=1
+            self.tableWidget.removeRow(0)
+            self.__row1 = 0
+            self.__max_row1 = 0
+            self.__column1 = 1
+    def actualizar2(self):
+        cant_row = self.tableWidget_2.rowCount()
+        self.listWidget_2.clear()
+        if(cant_row!=-0):
+            while(cant_row!=0):
+               self.tableWidget_2.removeRow(cant_row)
+               cant_row -=1
+            self.tableWidget_2.removeRow(0)
+            self.__row2 = 0
+            self.__max_row2 = 0
+            self.__column2 = 1
+
+    def insertRow1(self):
+
+         str = "SELECT ss_type,synset_offset,sense_number,tag_cnt FROM [index_sense] WHERE [lemma]='"+self.comboBox_2.currentText()+"' ORDER BY [ss_type],[sense_number]"
+         self.__cursor.execute(unicode(str))
+         result = self.__cursor.fetchall()
+
+         for con in result:
+             self.tableWidget.insertRow(self.__max_row1)
+             self.__max_row1 += 1
+
+         for a, b, c, d in result:
+            if(a==1):
+                self.tableWidget.setItem(self.__row1,0,QTableWidgetItem("noun"))
+                str = "SELECT [sense], [sense_es], [sense_long], [sense_long_es], [gloss], [gloss_es] FROM [data_noun] WHERE [synset_offset]='"+b+"'"
+                self.__cursor.execute(unicode(str))
+                data = self.__cursor.fetchall()
+
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][0]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][1]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][2]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][3]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][4]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][5]))
+            if(a==2):
+                self.tableWidget.setItem(self.__row1,0,QTableWidgetItem("verb"))
+                str = "SELECT [sense], [sense_es], [sense_long], [sense_long_es], [gloss], [gloss_es] FROM [data_verb] WHERE [synset_offset]='"+b+"'"
+                self.__cursor.execute(unicode(str))
+                data = self.__cursor.fetchall()
+
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][0]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][1]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][2]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][3]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][4]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][5]))
+            if(a==3 or a==5):
+                self.tableWidget.setItem(self.__row1,0,QTableWidgetItem("adj"))
+                str = "SELECT [sense], [sense_es], [sense_long], [sense_long_es], [gloss], [gloss_es] FROM [data_adj] WHERE [synset_offset]='"+b+"'"
+                self.__cursor.execute(unicode(str))
+                data = self.__cursor.fetchall()
+
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][0]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][1]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][2]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][3]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][4]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][5]))
+            if(a==4):
+                self.tableWidget.setItem(self.__row1,0,QTableWidgetItem("verb"))
+                str = "SELECT [sense], [sense_es], [sense_long], [sense_long_es], [gloss], [gloss_es] FROM [data_adv] WHERE [synset_offset]='"+b+"'"
+                self.__cursor.execute(unicode(str))
+                data = self.__cursor.fetchall()
+
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][0]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][1]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][2]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][3]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][4]))
+                self.__column1 += 1
+                self.tableWidget.setItem(self.__row1,self.__column1,QTableWidgetItem(data[0][5]))
+
+            self.__column1 = 1
+            self.__row1 += 1
+
+    def insertRow2(self):
+         str = "SELECT ss_type,synset_offset,sense_number,tag_cnt FROM [index_sense] WHERE [lemma]='"+self.comboBox_3.currentText()+"' ORDER BY [ss_type],[sense_number]"
+         self.__cursor.execute(unicode(str))
+         result = self.__cursor.fetchall()
+
+         for con in result:
+             self.tableWidget_2.insertRow(self.__max_row2)
+             self.__max_row2 += 1
+
+         for a, b, c, d in result:
+            if(a==1):
+                self.tableWidget_2.setItem(self.__row2,0,QTableWidgetItem("noun"))
+                str = "SELECT [sense], [sense_es], [sense_long], [sense_long_es], [gloss], [gloss_es] FROM [data_noun] WHERE [synset_offset]='"+b+"'"
+                self.__cursor.execute(unicode(str))
+                data = self.__cursor.fetchall()
+
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][0]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][1]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][2]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][3]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][4]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][5]))
+            if(a==2):
+                self.tableWidget_2.setItem(self.__row2,0,QTableWidgetItem("verb"))
+                str = "SELECT [sense], [sense_es], [sense_long], [sense_long_es], [gloss], [gloss_es] FROM [data_verb] WHERE [synset_offset]='"+b+"'"
+                self.__cursor.execute(unicode(str))
+                data = self.__cursor.fetchall()
+
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][0]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][1]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][2]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][3]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][4]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][5]))
+            if(a==3 or a==5):
+                self.tableWidget_2.setItem(self.__row2,0,QTableWidgetItem("adj"))
+                str = "SELECT [sense], [sense_es], [sense_long], [sense_long_es], [gloss], [gloss_es] FROM [data_adj] WHERE [synset_offset]='"+b+"'"
+                self.__cursor.execute(unicode(str))
+                data = self.__cursor.fetchall()
+
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][0]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][1]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][2]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][3]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][4]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][5]))
+            if(a==4):
+                self.tableWidget_2.setItem(self.__row2,0,QTableWidgetItem("verb"))
+                str = "SELECT [sense], [sense_es], [sense_long], [sense_long_es], [gloss], [gloss_es] FROM [data_adv] WHERE [synset_offset]='"+b+"'"
+                self.__cursor.execute(unicode(str))
+                data = self.__cursor.fetchall()
+
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][0]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][1]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][2]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][3]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][4]))
+                self.__column2 += 1
+                self.tableWidget_2.setItem(self.__row2,self.__column2,QTableWidgetItem(data[0][5]))
+
+
+            self.__column2 = 1
+            self.__row2 += 1
+
+    def buscarBook(self):
+       self.listWidget_3.clear()
+       self.textEdit.clear()
+       if(len(self.lineEdit_3.text()) != 0):
+        con = 1
+        while(open("./data/book/"+str(con)+".txt")):
+         #print con
+         book = open("./data/book/"+str(con)+".txt").read().lower()
+         #print book[book.find(self.lineEdit_3.text()):book.find(self.lineEdit_3.text())+len(self.lineEdit_3.text())]
+         nom_book = book[book.find("["):book.find("]")+1]
+         book = book[book.find("]")+1:book.__len__()]
+
+         palabra = self.lineEdit_3.text().toLower()
+         while(book.find(" "+palabra+" ")!= -1 ):
+          #if():
+           ini = book[0:book.find(" "+palabra+" ")].rfind(".")
+           if(ini == -1):
+               ini=0
+           fin = book[book.find(" "+palabra+" "):book.__len__()].find(".")+1
+
+           self.__bbook.append(book[ini:book.find(" "+palabra+" ")+fin])
+           print book[ini:book.find(" "+palabra+" ")+fin]
+
+           self.listWidget_3.addItem(nom_book +" "+ str(book[ini:book.find(" "+palabra+" ")+fin].__len__()))
+           book = book[book.find(" "+palabra+" ")+fin+1:book.__len__()]
+         con += 1
+
+    def Cargar(self):
+          self.textEdit.setText(self.__bbook[self.listWidget_3.currentRow()])
+    def actualizarbook(self):
+          self.listWidget_3.clear()
+          self.textEdit.clear()
+    def seleccionar(self):
+        self.__filasel = []
+        row = self.tableWidget_2.currentRow()
+
+        self.__filasel.append(self.tableWidget_2.takeItem(row,0))
+        self.__filasel.append(self.tableWidget_2.takeItem(row,1))
+        self.__filasel.append(self.tableWidget_2.takeItem(row,2))
+        self.__filasel.append(self.tableWidget_2.takeItem(row,3))
+        self.__filasel.append(self.tableWidget_2.takeItem(row,4))
+        self.__filasel.append(self.tableWidget_2.takeItem(row,5))
+        self.__filasel.append(self.tableWidget_2.takeItem(row,6))
+
+        self.actualizar2()
+        self.insertRow2()
+
+    def upsense(self):
+       if(self.__filasel.__len__() != 0):
+        self.tableWidget.insertRow(self.__max_row1)
+
+        self.tableWidget.setItem(self.__max_row1,0,QTableWidgetItem(self.__filasel[0]))
+        self.tableWidget.setItem(self.__max_row1,1,QTableWidgetItem(self.__filasel[1]))
+        self.tableWidget.setItem(self.__max_row1,2,QTableWidgetItem(self.__filasel[2]))
+        self.tableWidget.setItem(self.__max_row1,3,QTableWidgetItem(self.__filasel[3]))
+        self.tableWidget.setItem(self.__max_row1,4,QTableWidgetItem(self.__filasel[4]))
+        self.tableWidget.setItem(self.__max_row1,5,QTableWidgetItem(self.__filasel[5]))
+        self.tableWidget.setItem(self.__max_row1,5,QTableWidgetItem(self.__filasel[6]))
+        self.__max_row1 += 1
+
+        self.__filasel = []
+
+    def insertSynonyms(self):
+
+            self.listWidget.clear()
+            str = "SELECT [synset_offset] FROM [index_sense] WHERE [lemma]='"+self.comboBox_2.currentText()+"'"
+            self.__cursor.execute(unicode(str))
+            result = self.__cursor.fetchall()
+
+            if  result:
+             for w in result:
+                 #print w[2]
+                 str = "SELECT [word] FROM [data_noun_word_lex_id] WHERE [synset_offset]='"+w[0]+"'"
+                 self.__cursor.execute(unicode(str))
+                 data = self.__cursor.fetchall()
+                 if  data:
+                  for r in data:
+                      self.__filword.append(r[0])
+                  self.__allsynon.append(self.__filword)
+            print self.__allsynon[1][2]
+
+
