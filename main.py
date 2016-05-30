6a5d2092561a465662f4e9df168f04ee9a1e8ef8
Alexander Avello <aavello@uclv.cu>
2016-05-30 13:08:25 -0400

    cambios menores mientras voy corrigiendo.

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
 from PyQt4.QtCore import *
 from PyQt4.QtGui import QApplication
-
 from ui.QtNLP_Wordnet import Wordnet
+#from ui.QtNLP_Wordnet_Editor import WordnetEdit
 from ui.QtNLP_Wordnet_UI import Ui_MainWindow
-
 if __name__ == '__main__':
     """Función principal"""
     app = QApplication( sys.argv )
